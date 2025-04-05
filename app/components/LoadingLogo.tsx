37f27c8c619102281159c9d69feb1b5c08a033fa
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2025-04-06 01:12:50 +0800

    chore: push

--- app/components/LoadingLogo.tsx
+++ app/components/LoadingLogo.tsx
@@ -5,30 +5,27 @@ export default function LoadingLogo() {
     <div className='flex items-center justify-center w-full h-full bg-[#1e1e1e]'>
       <div className='relative w-32 h-32 animate-pulse-scale'>
         <Image
-          src='/icons/vib3_logo.svg'
+          src='/vib3_logo.png'
           alt='Vib3 Logo'
           layout='fill'
           objectFit='contain'
           priority
         />
       </div>
-      <style
-        jsx
-        global
-      >{`
+      <style jsx global>{`
         @keyframes pulseScale {
           0% {
-            transform: scale(1);
+            transform: scale(0.75);
           }
           50% {
-            transform: scale(1.15);
+            transform: scale(1);
           }
           100% {
-            transform: scale(1);
+            transform: scale(1.25);
           }
         }
         .animate-pulse-scale {
-          animation: pulseScale 6s ease-in-out infinite;
+          animation: pulseScale 2s ease-in-out infinite;
         }
       `}</style>
     </div>
